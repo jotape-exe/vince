3ca2b46e96845859ebbe1822c4cfad0a8ce8a725
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-07-25 10:12:31 -0300

    Category Spinner

--- app/src/main/java/com/company/ourfinances/view/fragments/RevenueFragment.kt
+++ app/src/main/java/com/company/ourfinances/view/fragments/RevenueFragment.kt
@@ -5,16 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import com.company.ourfinances.R
+import com.company.ourfinances.databinding.FragmentRevenueBinding
+import com.company.ourfinances.view.MainActivity
 
 class RevenueFragment : Fragment() {
+
+    private lateinit var binding: FragmentRevenueBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_revenue, container, false)
+    ): View {
+        binding = FragmentRevenueBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Dados irão vir do SQLite
+        val categoriesList = mutableListOf("Item 1","Item 2","item 3")
+        val adapter =
+            activity?.let { ArrayAdapter(it.applicationContext,
+                R.layout.style_spinner,categoriesList) }
+
+        binding.spinnerCategory.adapter = adapter
+
+        binding.spinnerCategory.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+    }
 
 }

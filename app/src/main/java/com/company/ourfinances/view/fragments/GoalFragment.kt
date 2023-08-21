0e3f29a287660bfe4170ff406e8850c5c8cec3ce
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-08-21 18:07:29 -0300

    FEAT environment variable config

--- app/src/main/java/com/company/ourfinances/view/fragments/GoalFragment.kt
+++ app/src/main/java/com/company/ourfinances/view/fragments/GoalFragment.kt
@@ -6,24 +6,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.company.ourfinances.R
 import com.company.ourfinances.databinding.FragmentGoalBinding
 import com.company.ourfinances.view.GoalManagerActivity
+import com.company.ourfinances.viewmodel.GoalViewModel
 
 
 class GoalFragment : Fragment() {
 
     private lateinit var binding: FragmentGoalBinding
+    private lateinit var viewModel: GoalViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGoalBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[GoalViewModel::class.java]
 
         listeners()
 
+        observers()
+
         return binding.root
+    }
+
+    private fun observers() {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.findAll()
     }
 
     private fun listeners() {
--- app/src/main/java/com/company/ourfinances/view/viewholder/GoalViewHolder.kt
+++ app/src/main/java/com/company/ourfinances/view/viewholder/GoalViewHolder.kt
@@ -3,6 +3,9 @@ package com.company.ourfinances.view.viewholder
 import androidx.recyclerview.widget.RecyclerView
 import com.company.ourfinances.databinding.GoalItemBinding
 
-class GoalViewHolder(private val bind: GoalItemBinding) : RecyclerView.ViewHolder(bind.root) {
+class GoalViewHolder(
+    private val bind: GoalItemBinding
+) :
+    RecyclerView.ViewHolder(bind.root) {
 
 }
